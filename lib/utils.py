b48f2652f6c36b7e9a9302c2143e703e7d098edd
Riccardo Bravin <riccardo.bravin.1@studenti.unipd.it>
2024-04-17 16:48:55 +0200

    reorganizing and fixing
    added v8 with positional encoder preceding fc and completed correct implementation of two new datasets

---
+++ lib/utils.py
@@ -0,0 +1,97 @@
+import torch
+from tqdm import tqdm
+
+
+# Print the model size
+def print_model_size(model):
+	param_size = 0
+	param_count = 0
+	for param in model.parameters():
+		param_size += param.nelement() * param.element_size()
+		param_count += param.nelement()
+	buffer_size = 0
+	for buffer in model.buffers():
+		buffer_size += buffer.nelement() * buffer.element_size()
+
+	size_all_mb = (param_size + buffer_size) / 1024**2
+	print('Model params: {:.3f}M'.format(param_count/1e6))
+	print('Model size: {:.3f}MB'.format(size_all_mb))
+
+
+
+def pad_sequences(ls, maxlen=512, truncating="post", padding="post", dtype="int"):
+	res = []
+	for innermost in ls[:]:
+		pad_len = max(0, maxlen - len(innermost))
+		res.append(innermost + [0] * pad_len)
+
+	return res
+	
+
+	
+
+def trainer(model, train_dataloader, lr, epochs):
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+	criterion = torch.nn.CrossEntropyLoss()
+	criterion.to(device);
+
+	optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+	for epoch in range(epochs):
+		model.train()
+		train_loss = 0
+		tqdm_train_loader = tqdm(train_dataloader, desc=f"Epoch {epoch+1}", leave=False)
+
+		for step_num, batch_data in enumerate(tqdm_train_loader):
+
+			token_ids, masks, labels = tuple(t.to(device) for t in batch_data)
+
+			logits = model(token_ids)
+
+			batch_loss = criterion(logits, labels)
+			train_loss += batch_loss.item()
+
+			model.zero_grad()
+			batch_loss.backward()
+
+
+			#torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+			optimizer.step()
+
+			log_step = 50
+			if step_num % log_step == (log_step - 1):
+				tqdm_train_loader.set_postfix(loss = train_loss / log_step)
+				train_loss = 0
+
+
+
+
+def evaluator(model, test_dataloader):
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+	criterion = torch.nn.CrossEntropyLoss()
+	criterion.to(device);
+
+	model.eval()
+	predicted = torch.Tensor([])
+	all_logits = torch.Tensor([])
+
+	tqdm_test_loader = tqdm(test_dataloader, desc=f"Evaluation: ", leave=False)
+	
+	avg_loss = 0
+	with torch.no_grad():
+		for step_num, batch_data in enumerate(tqdm_test_loader):
+
+			token_ids, masks, labels = tuple(t.to(device) for t in batch_data)
+
+			logits = model(token_ids)
+			loss = criterion(logits, labels)
+			avg_loss += loss.item()
+			numpy_logits = logits.cpu().detach()
+			predicted = torch.cat((predicted, torch.argmax(numpy_logits, dim = 1)))
+			all_logits  = torch.cat((all_logits, numpy_logits))
+	print("average loss: ", avg_loss / len(test_dataloader))
+	print()
+
+	return predicted.tolist();
--- main.py
+++ main.py
@@ -4,54 +4,69 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 
-from models import modelv7
-from models import utils
+from lib import modelv0
+from lib import modelv7
+from lib import modelv8
 
-from models.dataset import dataset_importer
-from models import utils
+from lib import utils
+
+from lib.dataset import dataset_importer
+from lib import utils
 
 VOCAB_SIZE = 512*8
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 
 REDUCED_EMBEDDING_DIM = 16
-EMBED_DIM = 124
+EMBED_DIM = 128
 NUM_HEADS = 8
-FORWARD_EXPANSION = 0.1
-MAX_LENGTH = 512
-LAYERS = 1
+FORWARD_EXPANSION = 2
+MAX_LENGTH = 128
+LAYERS = 2
 
 
+
+
+########################################################################################
 print("Loading dataset:")
 
-#'Amazon', "imdb", "sst2" "sst5" "twitter"
-DATASET = "imdb"
+#'Amazon', "imdb", "sst2" "sst5" "twitter" "race" "yelp"
+DATASET = "sst2"
 
 train_dataloader, test_dataloader, N_LABELS, label_test = dataset_importer(DATASET, VOCAB_SIZE, MAX_LENGTH, BATCH_SIZE)
 
+
+########################################################################################
 print("Model initialization:")
 #initialize model
-classifier = modelv7.Classifier(VOCAB_SIZE, MAX_LENGTH, REDUCED_EMBEDDING_DIM, EMBED_DIM, NUM_HEADS, FORWARD_EXPANSION, LAYERS, N_LABELS)
+#classifier = modelv0.ClassifierV0(VOCAB_SIZE, MAX_LENGTH, EMBED_DIM, NUM_HEADS, FORWARD_EXPANSION, N_LABELS)
+#classifier = modelv7.ClassifierV7(VOCAB_SIZE, MAX_LENGTH, REDUCED_EMBEDDING_DIM, EMBED_DIM, NUM_HEADS, FORWARD_EXPANSION, LAYERS, N_LABELS)
+classifier = modelv8.ClassifierV8(VOCAB_SIZE, MAX_LENGTH, REDUCED_EMBEDDING_DIM, EMBED_DIM, NUM_HEADS, FORWARD_EXPANSION, LAYERS, N_LABELS)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 classifier.to(device)
+print(f"Model initialized on {device}")
+
 
 print("Model parameters:")
 #print all model parameters with names
 for name, param in classifier.named_parameters():
-  print(f"{name}: {param.nelement()}")
+	print(f"{name}: {param.nelement()}")
 
 #print the model size
 utils.print_model_size(classifier)
 
 
 
-
+########################################################################################
 print("Starting training")
+
 EPOCHS = 10
 LR = 2e-4
 
-
 utils.trainer(classifier, train_dataloader, LR, EPOCHS)
 
+########################################################################################
+print("Starting evaluation")
 predicted = utils.evaluator(classifier, test_dataloader)
 
 print(classification_report(label_test, predicted))
@@ -60,6 +75,8 @@ conf_matrix = confusion_matrix(label_test, predicted)
 
 print("Confusion matrix:\n", conf_matrix)
 
-# Print or visualize the confusion matrix
-# print("Confusion Matrix:")
-# print(conf_matrix)
+
+########################################################################################
+#save model
+t_string = "./models/transformer_v7_" + str(REDUCED_EMBEDDING_DIM) + "_" + str(EMBED_DIM) + "_" + str(FORWARD_EXPANSION) + "_" + str(LAYERS) + "_" + str(VOCAB_SIZE) + "_" + str(MAX_LENGTH) + "_" + str(BATCH_SIZE) + "_" + str(EPOCHS) + "_" + str(DATASET) + ".pt"
+torch.save(classifier.state_dict(), t_string)
