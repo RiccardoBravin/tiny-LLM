Fixed accuracy calculation of pretrainer bert

BERT_pretrain.py
@@ -21,8 +21,8 @@ epochs_pretraining = 10
 lr_pretraining = 5e-3
 
 epochs_post = 10
-lr_post = 1e-2
-logs_x_epoch = 1
+lr_post = 1e-5
+logs_x_epoch = 2
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -30,7 +30,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 dataset_config = DataConfig(
 					dataset_name=None, 
 					dict_size=pow(2, 12), 
-					tokenizer_type="wordpiece", 
+					tokenizer_type="bpe", 
 					batch_size=32, 
 					max_len=64, 
 					labels=None
@@ -50,7 +50,7 @@ model_config = ModelConfig(
 
 ########################################################################################
 
-for DATASET_NAME in ["imdb", "sst2", "news", "bull", "limit", "nlu", "snips", "nli"]:
+for DATASET_NAME in ["sst2", "news", "bull", "limit", "nlu", "snips", "nli", "imdb"]:
 	dataset_config.dataset_name = DATASET_NAME
 
 	#load the dataset   
@@ -139,7 +139,7 @@ for DATASET_NAME in ["imdb", "sst2", "news", "bull", "limit", "nlu", "snips", "n
 
 		print(f"{FOREGROUND_COLORS["BrightYellow"]}Testing the model{RESET}")
 		print(f"{FOREGROUND_COLORS["BrightCyan"]}", end="")
-		predicted, avg_eval_loss = trainer.evaluate(classifier, test_dataloader)
+		predicted, avg_eval_loss = trainer.evaluate(test_dataloader)
 		print(f"{RESET}")
 
 

lib/trainer.py
@@ -28,8 +28,8 @@ def mask_tokens(tokens, dict_size, device, mlm_prob = 0.15):
 	
 	
 	masked_indices = torch.bernoulli(mlm_probability).bool() # 15% of the tokens are 1 and 85% are 0
-	randomized_indices = torch.bernoulli(masked_indices * 0.2).bool().to(device) # 20% of the 15% are 1 and 80% are 0
-
+	randomized_indices = torch.bernoulli(masked_indices * 0.4).bool().to(device) # 40% of the 15% are 1 and 60% are 0
+	
 	masked_tokens = tokens.clone()
 	masked_tokens[masked_indices * (~randomized_indices)] = 3 # [MASK] token is 3
 	masked_tokens[masked_indices * randomized_indices] = torch.randint(5, dict_size, masked_tokens[masked_indices * randomized_indices].shape).to(device) # random token in the dictionary
@@ -84,6 +84,7 @@ class BertTrainer:
 		self.optimizer = AdamW(lr=lr, params=self.model.parameters())
 		self.device = device 
 		self.lm_criterion = nn.CrossEntropyLoss(ignore_index=0).to(device)
+		self.cls_criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([15])).to(device)
 		self.model_config = model_config
 
 	def train(self, train_dataloader, eval_dataloader, num_epochs, log_freq: int, color = FOREGROUND_COLORS['Green']):
@@ -113,7 +114,7 @@ class BertTrainer:
 					logits, label_guess = self.model(masked_tokens, masks) 
 			
 					lm_loss = self.lm_criterion(logits.transpose(1,2), tokens)
-					cls_loss = nn.functional.binary_cross_entropy_with_logits(label_guess.squeeze(-1), cls_labels.float(), pos_weight=torch.tensor([15]).to(self.device))
+					cls_loss = self.cls_criterion(label_guess.squeeze(-1), cls_labels.float())
 
 					batch_loss = lm_loss + cls_loss
 
@@ -156,16 +157,18 @@ class BertTrainer:
 						masked_tokens, cls_labels = mask_tokens(tokens, self.model_config.vocab_size, self.device)
 
 						logits, label_guess = self.model(masked_tokens, masks)
-						
+						label_guess = label_guess.squeeze(-1)
+
 						lm_loss = self.lm_criterion(logits.transpose(1,2), tokens)
-						cls_loss = nn.functional.binary_cross_entropy_with_logits(label_guess.squeeze(-1), cls_labels.float(), pos_weight=torch.tensor([15]).to(self.device))
+						cls_loss = nn.functional.binary_cross_entropy_with_logits(label_guess, cls_labels.float(), pos_weight=torch.tensor([15]).to(self.device))
 
 						val_loss += lm_loss.item() + cls_loss.item()
 						cls_avg_loss += cls_loss.item()
 						mlm_avg_loss += lm_loss.item()
-
+						
 						mlm_accuracy = torch.cat((mlm_accuracy, (torch.argmax(logits, dim=2) == tokens)))
-						cls_accuracy = torch.cat((cls_accuracy, (torch.argmax(label_guess, dim=2) == cls_labels)))
+						cls_accuracy = torch.cat((cls_accuracy, ((label_guess > 0.5) == cls_labels.float())))
+					
 
 					mlm_accuracy = torch.mean(mlm_accuracy.float()).item()					
 					cls_accuracy = torch.mean(cls_accuracy.float()).item()